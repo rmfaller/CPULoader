cd8a8f2d5c23bb132bf9299d0caff544d0efcc48
rmfaller <robert.m.faller@gmail.com>
2017-10-06 16:22:19 -0700

    reference-architecture-00

--- src/cpuloader/CPULoader.java
+++ src/cpuloader/CPULoader.java
@@ -5,6 +5,8 @@
  */
 package cpuloader;
 
+import java.util.Date;
+
 /**
  *
  * @author rmfaller
@@ -16,6 +18,7 @@ public class CPULoader extends Thread {
     private static int threshold = 0;
     private static int defaultthreshold = 5;
     private static boolean csv = false;
+    private static boolean forever = false;
 
     /**
      * @param args the command line arguments
@@ -44,6 +47,10 @@ public class CPULoader extends Thread {
                 case "--csv":
                     csv = true;
                     break;
+                case "-f":
+                case "--forever":
+                    forever = true;
+                    break;
                 case "-h":
                 case "--help":
                     help();
@@ -70,42 +77,54 @@ public class CPULoader extends Thread {
                 spinner.continueToRun = false;
                 System.out.println(". Stopped and completed.");
             } else {
-                long threadcnt = threads;
-                long tasktime = 0;
+                int loops = 0;
+                while (loops <= 1) {
+                    long threadcnt = threads;
+                    long tasktime = 0;
 //                System.out.println("CPULoader found " + cores + " cores and starting with " + threadcnt + " thread(s) and not to exceed " + lapsedtime + "ms total loop time per thread");
-                if (csv) {
-                    System.out.println("threads,avr-time,passed,exceeded,threshold,");
-                } else {
-                    System.out.println("CPULoader found " + cores + " cores and starting with " + threadcnt + " thread(s) and not to exceed " + lapsedtime + "ms total loop time per thread");
-                }
-                while (threshold >= (tasktime / (float) threadcnt)) {
-                    Loaders[] loaders = new Loaders[(int) threadcnt];
-                    for (int i = 0; i < threadcnt; i++) {
-                        loaders[i] = new Loaders(i, lapsedtime, threshold);
-                        loaders[i].start();
-                    }
-                    tasktime = 0;
-                    int exceed = 0;
-                    int passed = 0;
-                    for (int i = 0; i < threadcnt; i++) {
-                        loaders[i].join();
-                        tasktime = loaders[i].getTaskTime() + tasktime;
-                        if (loaders[i].getTaskTime() > threshold) {
-                            exceed++;
-                        } else {
-                            passed++;
+                    if (!csv) {
+                        System.out.println("CPULoader found " + cores + " cores and starting with " + threadcnt + " thread(s) and not to exceed " + lapsedtime + "ms total loop time per thread");
+                    } else {
+                        if (loops == 0) {
+                            System.out.println("timestamp,threads,avr-time,passed,exceeded,threshold,");
                         }
                     }
-                    if (csv) {
-                        System.out.println(threadcnt + "," + (float) (tasktime / (float) threadcnt) + "," + passed + "," + exceed + "," + threshold);
-                    } else {
-                        System.out.print("Thread count = ");
-                        System.out.format("%4d", threadcnt);
-                        System.out.print(" with average time to run = ");
-                        System.out.format("%5.2f", (float) (tasktime / (float) threadcnt));
-                        System.out.println("ms and " + exceed + " exceeded the " + threshold + "ms threshold");
+                    while (threshold >= (tasktime / (float) threadcnt)) {
+                        Loaders[] loaders = new Loaders[(int) threadcnt];
+                        for (int i = 0; i < threadcnt; i++) {
+                            loaders[i] = new Loaders(i, lapsedtime, threshold);
+                            loaders[i].start();
+                        }
+                        tasktime = 0;
+                        int exceed = 0;
+                        int passed = 0;
+                        for (int i = 0; i < threadcnt; i++) {
+                            loaders[i].join();
+                            tasktime = loaders[i].getTaskTime() + tasktime;
+                            if (loaders[i].getTaskTime() > threshold) {
+                                exceed++;
+                            } else {
+                                passed++;
+                            }
+                        }
+                        if (csv) {
+                            System.out.println((long) new Date().getTime() + "," + threadcnt + "," + (float) (tasktime / (float) threadcnt) + "," + passed + "," + exceed + "," + threshold);
+                        } else {
+                            System.out.print("Thread count = ");
+                            System.out.format("%4d", threadcnt);
+                            System.out.print(" with average time to run = ");
+                            System.out.format("%5.2f", (float) (tasktime / (float) threadcnt));
+                            System.out.println("ms and " + exceed + " exceeded the " + threshold + "ms threshold");
+                        }
+                        threadcnt++;
                     }
-                    threadcnt++;
+                    if (!forever) {
+                        loops = 2;
+                    } else {
+                        loops = 1;
+                        threadcnt = threads;
+                        tasktime = 0;
+                    }
                 }
             }
         }
@@ -115,10 +134,11 @@ public class CPULoader extends Thread {
         String help = "\nCPULoader usage:"
                 + "\njava -jar ./dist/CPULoader.jar"
                 + "\navailable options:"
-                + "\n\t--lapsedtime | -l {default = " + lapsedtime + "} time, in milliseconds the load should run"
-                + "\n\t--threads    | -t {default = " + threads + "} numbers of threads to spawn"
+                + "\n\t--lapsedtime | -l {default = " + lapsedtime + "} time, in milliseconds the load should run - OR - how many iterations a thread should make when used with --forever"
+                + "\n\t--threads    | -t {default = " + threads + "} maximum number of threads to spawn"
                 + "\n\t--threshold  | -s {default = " + defaultthreshold + "} milliseconds a thread must complete by; once exceeded stop the thread"
-                + "\n\t--csv        | -c {default = non-csv output} outputs in comma-delimited format when using --threshold"
+                + "\n\t--csv        | -c {default = non-csv output} outputs in comma-delimited format"
+                + "\n\t--forever    | -f {default is to NOT run forever}"
                 + "\n\t--help       | -h this output\n"
                 + "\nExample: java -jar ./dist/CPULoader.jar --lapsedtime 20000 --threads 2\n";
         System.out.println(help);
